197f737021a6895f89f92f6abd5add5071a51853
USwampertor <idv16c.mmillan@uartesdigitales.edu.mx>
2018-10-20 23:59:23 -0500

    +Player.cpp +Player.h +Tile.cpp +Tile.h +World.cpp +World.cpp *Vector2

---
+++ AI2_2017/AI2_2017/World.h
@@ -0,0 +1,29 @@
+/*||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||*/
+/**
+ * @file World.h
+ * @author Marco "Swampy" Millan
+ * @date 2018/10/20 2018
+ * @brief 
+ * 
+ */
+/*||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||°°°||*/
+#pragma once
+
+#include "Utilities.h"
+#include "Unit.h"
+#include "UnitType.h"
+class World
+{
+public:
+  World() = default;
+  ~World();
+
+ public:
+  
+   std::vector<Unit> m_unitsInGame;
+
+   std::vector<UnitType> m_unitTypes;
+
+
+};
+
